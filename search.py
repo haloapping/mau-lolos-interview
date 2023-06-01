21bc0584caa5a687eb97239c3a5f7ea8009482ae
Alfiyanto Kondolele <alfiyanto.kondolele@gmail.com>
2023-06-01 20:21:52 +0700

    refactor return value

--- search.py
+++ search.py
@@ -1,15 +1,17 @@
 from typing import Any
+
 import shuffle
 
-def linear_search(collection: list[Any], search: Any) -> bool:
+
+def linear_search(collection: list[Any], search: Any) -> tuple[bool, int]:
     for idx, item in enumerate(collection):
         if item == search:
-            return True
+            return True, idx
 
-    return False
+    return False, -1
 
 
-def binary_search(collection: list[Any], search: Any) -> bool:
+def binary_search(collection: list[Any], search: Any) -> tuple[bool, int]:
     left_idx: int = 0
     right_idx: int = len(collection) - 1
 
@@ -17,26 +19,26 @@ def binary_search(collection: list[Any], search: Any) -> bool:
         mid_idx: int = (left_idx + right_idx) // 2
 
         if search == collection[mid_idx]:
-            return True
+            return True, mid_idx
         elif search > collection[mid_idx]:
             left_idx = mid_idx + 1
         else:
             right_idx = mid_idx - 1
 
-    return False
+    return False, -1
 
 
 def binary_search_recursive(
     collection: list[Any], search: Any, left_idx: int, right_idx: int
-) -> bool:
+) -> tuple[bool, int]:
     if left_idx <= right_idx:
         mid_idx: int = (left_idx + right_idx) // 2
 
         if search == collection[mid_idx]:
-            return True
+            return True, mid_idx
         elif search > collection[mid_idx]:
             return binary_search_recursive(collection, search, mid_idx + 1, right_idx)
         else:
             return binary_search_recursive(collection, search, left_idx, mid_idx - 1)
     else:
-        return False
+        return False, -1
